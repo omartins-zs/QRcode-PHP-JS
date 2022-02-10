3a7cdab83d94ff023af29a346612c8083aa8f703
omartins-zs <gabrielmatheus209@gmail.com>
2022-02-10 08:52:22 -0300

    Adicionando Script do Banco de Dados

---
+++ QRcode-PHP-JS.sql
@@ -0,0 +1,7 @@
+CREATE TABLE `qrcodephpjs`.`links` ( `id` INT NOT NULL AUTO_INCREMENT , `description` VARCHAR(150) NOT NULL , `link` VARCHAR(150) NOT NULL , PRIMARY KEY (`id`)) ENGINE = InnoDB;
+
+
+
+INSERT INTO `links` (`id`, `description`, `link`) VALUES (NULL, '10 apps que CONSERTAM o Windows 11 (Menu iniciar, Barra de tarefas, Menu de Contexto, e muito mais)\r\n', 'https://www.youtube.com/watch?v=s5SuRqzd5IU&ab_channel=CanalWINchester'), (NULL, 'NOVO GERENCIADOR DE TAREFAS [COM DARK THEME!!! ] - Windows 11 Build 22538\r\n', 'https://www.youtube.com/watch?v=gI01OHvvv5g&ab_channel=CanalWINchester')
+
+INSERT INTO `links` (`id`, `description`, `link`) VALUES (NULL, 'MAIS 7 ferramentas INCRÍVEIS que vão MODERNIZAR o seu Windows 10! [Parte 2]\r\n', 'https://www.youtube.com/watch?v=7qhHegwt6zo&ab_channel=CanalWINchester')
